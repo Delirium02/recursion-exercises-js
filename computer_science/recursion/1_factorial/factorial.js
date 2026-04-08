fc9fe06831066d1151366fb4d6fb899430f67d6f
delirium02 <darknightbs@gmail.com>
2026-04-09 07:19:27 +1000

    All tests passed

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,10 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    if (n === 0) return 1;
+    else if (!Number(n) || n < 0 || !Number.isInteger(n)) {
+        return undefined;
+    } else {
+        return n * factorial(n - 1);
+    }
 };
 
 // Do not edit below this line
